f9eb5840a63564c5bc985ebc85773e19d6e8921a
Santiago Rivera <santiagoraika@gmail.com>
2024-10-10 21:50:44 -0500

    first commit

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+var path = require('path'); // Asegúrate de importar 'path'
+var puppeteer = require('puppeteer');
+
+process.env.CHROME_BIN = puppeteer.executablePath();
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    coverageReporter: {
+      dir: path.join(__dirname, './coverage/my-app'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'], // Usa ChromeHeadless
+    singleRun: true,
+    restartOnFileChange: true,
+  });
+};
